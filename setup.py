3ea562cad97bc1cff2a91f1b595c8c6ae2e46dc7
Dmitry Shachnev <mitya57@gmail.com>
2013-01-02 15:50:12 +0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = \
 '''This module provides a way for securely storing passwords and other secrets.
 
 It uses DBus Secret Service API that is supported by GNOME Keyring (>= 2.30) and
-KWallet (>= 4.8).
+KSecretsService.
 
 It allows one to create new passwords, delete and search for passwords matching
 given attributes. It also supports graphical prompts when unlocking is needed.'''
@@ -31,7 +31,7 @@ classifiers = ['Development Status :: 4 - Beta',
 
 setup(name='SecretStorage',
 	version=version,
-	description='Secure passwords storage',
+	description='Secure storing of passwords and another secrets',
 	long_description=long_description,
 	author='Dmitry Shachnev',
 	author_email='mitya57@gmail.com',
