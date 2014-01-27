3ac452d448b591fbd0de712f622ecf4ec35e2f55
Dmitry Shachnev <mitya57@gmail.com>
2014-01-27 22:29:17 +0400

    Releasing version 2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os.path
 from distutils.core import setup
 
-version = '1.1.0'
+version = '2.0.0'
 
 readme_file = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 long_description = '\n' + readme_file.read()
