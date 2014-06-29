ce844f476f7c528b421641b9a03ac5ef5a9453d2
Dmitry Shachnev <mitya57@gmail.com>
2014-06-29 11:30:13 +0400

    Add Python 3.4 to supported versions

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ classifiers = ['Development Status :: 4 - Beta',
 	'Programming Language :: Python :: 3.1',
 	'Programming Language :: Python :: 3.2',
 	'Programming Language :: Python :: 3.3',
+	'Programming Language :: Python :: 3.4',
 	'Topic :: Security',
 	'Topic :: Software Development :: Libraries :: Python Modules'
 ]
