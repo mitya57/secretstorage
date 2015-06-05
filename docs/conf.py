0ceb3136d52d4f92b454f494b4cccc549bbb8778
Dmitry Shachnev <mitya57@gmail.com>
2015-06-05 19:39:51 +0300

    docs: Switch to alabaster theme (the new Sphinx default)

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'SecretStorage'
-copyright = '2013-2014, Dmitry Shachnev'
+copyright = '2015, Dmitry Shachnev'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
