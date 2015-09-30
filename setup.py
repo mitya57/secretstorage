0f549e6924380922541ed662d3cd922a84f99d60
Dmitry Shachnev <mitya57@gmail.com>
2015-09-30 18:13:30 +0300

    setup.py: Remove 2.6 from supported Python versions and add 3.5

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ classifiers = [
 	'Operating System :: POSIX',
 	'Programming Language :: Python',
 	'Programming Language :: Python :: 2',
-	'Programming Language :: Python :: 2.6',
 	'Programming Language :: Python :: 2.7',
 	'Programming Language :: Python :: 3',
 	'Programming Language :: Python :: 3.1',
 	'Programming Language :: Python :: 3.2',
 	'Programming Language :: Python :: 3.3',
 	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
 	'Topic :: Security',
 	'Topic :: Software Development :: Libraries :: Python Modules'
 ]
