9ae74c0c44abc492608041bc0af5fd00c5968fc5
Dmitry Shachnev <mitya57@gmail.com>
2016-02-10 22:46:42 +0300

    Add run_tests.py script that can use libsecret's mock

---
+++ tests/run_tests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+import subprocess
+import unittest
+
+tests_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.dirname(tests_dir))
+
+import secretstorage
+
+if __name__ == '__main__':
+	print('Running with Python %d.%d.%d (SecretStorage from %s)' %
+	      (sys.version_info[:3] + (os.path.dirname(secretstorage.__file__),)))
+	mock = None
+	if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
+		mock = subprocess.Popen(('python3', sys.argv[1],),
+		                        stdout=subprocess.PIPE,
+		                        universal_newlines=True)
+		bus_name = mock.stdout.readline().rstrip()
+		secretstorage.util.BUS_NAME = bus_name
+		print('Bus name set to %r' % secretstorage.util.BUS_NAME)
+	loader = unittest.TestLoader()
+	runner = unittest.TextTestRunner(verbosity=2)
+	result = runner.run(loader.discover(tests_dir))
+	if mock is not None:
+		mock.terminate()
+	sys.exit(not result.wasSuccessful())
