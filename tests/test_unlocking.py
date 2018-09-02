0b32ae7a09dd961f379104b6628eaf5700cca785
Dmitry Shachnev <mitya57@gmail.com>
2018-09-02 21:56:49 +0300

    Add test coverage for Item.ensure_not_locked() method

--- tests/test_unlocking.py
+++ tests/test_unlocking.py
@@ -4,7 +4,7 @@
 
 import unittest
 
-from secretstorage import dbus_init, get_any_collection
+from secretstorage import dbus_init, Collection
 from secretstorage.util import BUS_NAME
 from secretstorage.exceptions import LockedException
 
@@ -14,12 +14,16 @@ from secretstorage.exceptions import LockedException
 class LockingUnlockingTest(unittest.TestCase):
 	def setUp(self) -> None:
 		self.connection = dbus_init()
-		self.collection = get_any_collection(self.connection)
+		collection_path = "/org/freedesktop/secrets/collection/english"
+		self.collection = Collection(self.connection, collection_path)
 
 	def test_lock_unlock(self) -> None:
+		self.assertFalse(self.collection.is_locked())
 		self.collection.lock()
 		self.assertTrue(self.collection.is_locked())
 		self.assertRaises(LockedException, self.collection.ensure_not_locked)
+		item, = self.collection.search_items({"number": "1"})
+		self.assertRaises(LockedException, item.ensure_not_locked)
 		self.assertIs(self.collection.unlock(), False)
 		self.assertFalse(self.collection.is_locked())
 		self.collection.ensure_not_locked()
