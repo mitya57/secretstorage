2895ebed7310ef404c3bdaf72ebff9ba52f09f7d
Dmitry Shachnev <mitya57@gmail.com>
2020-11-24 15:52:31 +0300

    Releasing version 3.3.0
    Fixes #26.

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,9 @@ copyright = '2020, Dmitry Shachnev'
 # built documents.
 #
 # The short X.Y version.
-version = '3.2'
+version = '3.3'
 # The full version, including alpha/beta/rc tags.
-release = '3.2.0'
+release = '3.3.0'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
