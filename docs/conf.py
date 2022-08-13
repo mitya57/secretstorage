3993fc7162fe50852852913b90b203ba13727c38
Dmitry Shachnev <mitya57@gmail.com>
2022-08-13 19:16:20 +0300

    Releasing version 3.3.3

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = '2022, Dmitry Shachnev'
 # The short X.Y version.
 version = '3.3'
 # The full version, including alpha/beta/rc tags.
-release = '3.3.2'
+release = '3.3.3'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
